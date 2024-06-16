3ce6ddeea305e0011bbc2f56929d9690fd880cac
dk5042 <damianknopek03@gmail.com>
2024-06-16 14:53:55 +0200

    shop checkboxes working

---
+++ JRPG/Model/ItemWithCheckBox.cs
@@ -0,0 +1,15 @@
+﻿using JRPG.DAL.Encje;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JRPG.Model
+{
+    class ItemWithCheckBox
+    {
+        public Items Item { get; set; }
+        public bool IsChecked { get; set; }
+    }
+}
--- JRPG/ViewModel/ShopScreenVM.cs
+++ JRPG/ViewModel/ShopScreenVM.cs
@@ -7,6 +7,8 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
 
 namespace JRPG.ViewModel
 {
@@ -14,11 +16,16 @@ namespace JRPG.ViewModel
     {
         private MainModel model;
         private ObservableCollection<Items> items = new ObservableCollection<Items>();
+        private ObservableCollection<ItemWithCheckBox> itemsWithCheckBox = new ObservableCollection<ItemWithCheckBox>();
 
         public ShopScreenVM(MainModel mainModel)
         {
             model = mainModel;
             items = model.itemsModel.AllItems;
+            foreach (var item in items)
+            {
+                ItemsWithCheckBox.Add(new ItemWithCheckBox { Item = item, IsChecked = false });
+            }
         }
         
         public ObservableCollection<Items> Items 
@@ -29,6 +36,39 @@ namespace JRPG.ViewModel
                 value = items;
                 onPropertyChanged(nameof(Items)); 
             } 
+        }
+        public ObservableCollection<ItemWithCheckBox> ItemsWithCheckBox
+        { 
+            get { return itemsWithCheckBox; }
+            set
+            {
+                itemsWithCheckBox = value;
+                onPropertyChanged(nameof(ItemsWithCheckBox));
+            }
+        }
+
+        private ICommand buttonBuy = null;
+        public ICommand ButtonBuy
+        {
+            get
+            {
+                if (buttonBuy == null)
+                {
+                    buttonBuy = new RelayCommand(
+                        arg =>
+                        {
+                            string x = "";
+                            for (int i = 0; i < ItemsWithCheckBox.Count; i++)
+                            {
+                                x += ItemsWithCheckBox[i].IsChecked.ToString();
+                                x += "\n";
+                            }
+                            MessageBox.Show(x);
+                        },
+                        arg => true);
+                }
+                return buttonBuy;
+            }
         }
     }
 }
